53643c76007187539807a0e10f82e7302627623b
doodlebro <jess.boerner@fullcontact.com>
2019-03-03 10:47:48 -0700

    Multi-commit:
    Main planned
    Add routine to load CSV to list
    Add release date to movieHeader

--- hydrate_model.py
+++ hydrate_model.py
@@ -4,9 +4,26 @@ import csv
 # Pre-vars
 movieMDFilename = 'movies_metadata.csv'
 
-movieHeader = ['movie_id', 'original_title', 'budget', 'revenue', 'popularity']
+movieHeader = ['movie_id', 'original_title', 'release_date', 'budget', 'revenue', 'popularity']
 genreHeader = ['genre_id', 'movie_id', 'name']
 pcHeader = ['pc_id', 'movie_id', 'name']
 
+def loadCSV(filename):
+	file = open(filename, 'rb')
+	reader = csv.reader(file)
+	csvList = list(reader)
+
+	return csvList
+
+
 def mane():
+	# Load CSV as list
+	
+	
+	# Hold onto header
+	
+	
+	# Iterate through metadata
+	
+	
 	print 'done!'
