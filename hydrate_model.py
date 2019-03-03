986861f59f8701fea1616700377f36c1081d5cfc
doodlebro <jess.boerner@fullcontact.com>
2019-03-03 10:42:57 -0700

    Initial commit for hydration implementation

---
+++ hydrate_model.py
@@ -0,0 +1,12 @@
+import csv
+
+
+# Pre-vars
+movieMDFilename = 'movies_metadata.csv'
+
+movieHeader = ['movie_id', 'original_title', 'budget', 'revenue', 'popularity']
+genreHeader = ['genre_id', 'movie_id', 'name']
+pcHeader = ['pc_id', 'movie_id', 'name']
+
+def mane():
+	print 'done!'
